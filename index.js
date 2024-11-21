ce0c668690c52581d4761c90e798657e572fece6
Enzo Esmeraldo <enzopo625@gmail.com>
2024-11-20 23:24:06 -0300

    // updating images and resume

--- index.js
+++ index.js
@@ -44,5 +44,5 @@ window.addEventListener("scroll", () => {
 
 
 function openPDF() {
-  window.open('images/curriculo2024-enzo.pdf', '_blank');
+  window.open('images/Curricul_Enzo_Esmeraldo.pdf', '_blank');
 }
